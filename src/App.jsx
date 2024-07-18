24c0ba8579a9a4695c22fa63c4bbf5b3b032fecf
Theo <asakvsa.idn@gmail.com>
2024-07-18 18:00:15 +0700

    project init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<section className="relative w-full h-screen">
+			<div className="relative flex items-center justify-center w-full h-full z-10"></div>
+		</section>
+	);
+}
