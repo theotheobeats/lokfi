f575cdc69b49dccf4d2f0d6b767774c17b992e0d
Theo <asakvsa.idn@gmail.com>
2024-07-19 15:53:40 +0700

    project mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,78 @@
+import { MusicIcon, PauseCircle, PlayCircle } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+function updateTime() {
+	const now = new Date();
+	const hours = String(now.getHours()).padStart(2, 0);
+	const minute = String(now.getMinutes()).padStart(2, 0);
+	const second = String(now.getSeconds()).padStart(2, 0);
+
+	const currentTime = `${hours}:${minute}:${second}`;
+
+	return currentTime;
+}
+
 export default function App() {
+	const [time, setTime] = useState(updateTime);
+	const audioRef = useRef(null);
+	const [isPlay, setIsPlay] = useState(false);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTime(updateTime());
+		}, 1000);
+
+		return () => clearInterval(intervalId);
+	}, []);
+
+	const playAudio = () => {
+		if (audioRef.current) {
+			audioRef.current.play();
+			setIsPlay(true);
+		}
+	};
+
+	const pauseAudio = () => {
+		if (audioRef.current) {
+			audioRef.current.pause();
+			setIsPlay(false);
+		}
+	};
+
 	return (
 		<section className="relative w-full h-screen">
-			<div className="relative flex items-center justify-center w-full h-full z-10"></div>
+			<img
+				src="../src/assets/bg1.jpg"
+				className="z-0 fixed w-full h-screen object-cover"
+			/>
+			<div className="relative flex items-center justify-center w-full h-full z-10">
+				<div className="flex-col">
+					<div className="backdrop-blur-xl bg-white/5 flex p-4 rounded-2xl shadow-xl">
+						<div className="text-[4rem] font-bold text-white pr-8 pl-6 pb-2">
+							{time}
+						</div>
+					</div>
+					<div className="flex items-center justify-center m-4 text-white gap-4">
+						{isPlay ? (
+							<PauseCircle
+								className="hover:text-slate-400 cursor-pointer"
+								onClick={pauseAudio}
+							/>
+						) : (
+							<MusicIcon
+								className="hover:text-slate-400 cursor-pointer"
+								onClick={playAudio}
+							/>
+						)}
+
+						<PlayCircle className="hover:text-slate-400 cursor-pointer" />
+						<div>
+							Pomodoro <span className="text-green-300">Active</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<audio src="../src/assets/bgm1.mp3" ref={audioRef} />
 		</section>
 	);
 }
